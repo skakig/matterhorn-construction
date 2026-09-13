283979a8baae2b2e018b22c006a275bff68fe4aa
Joshua Ellis <skakig@gmail.com>
2026-09-13 13:13:46 -0600

    Simplify experience scroller wording

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -25,7 +25,7 @@ export const stats = [
   { value: "12,658", unit: "ft", label: "Pagosa Peak" },
   { value: "1,002", unit: "ft", label: "World’s deepest hot spring" },
   { value: "4th", unit: "generation", label: "Master builder" },
-  { value: "40+", unit: "years", label: "Jody Ellis’s professional construction experience" },
+  { value: "40+", unit: "years", label: "of professional construction experience" },
 ];
 
 export const phases = [
